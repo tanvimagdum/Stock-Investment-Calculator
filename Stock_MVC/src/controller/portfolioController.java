3ff8601a51046f2facbf3fc0b60b887a7baff50e
tanvimagdum <magdum.t@northeastern.edu>
2022-10-29 13:36:30 -0400

    new commit 1.36

--- Stock_MVC/src/controller/portfolioController.java
+++ Stock_MVC/src/controller/portfolioController.java
@@ -1,16 +1,18 @@
 package controller;
 
+import model.Pair;
 import model.portfolio;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 
 public interface portfolioController {
 
-    public void portBuilder(String ticker, float count);
+    public void portBuilder(ArrayList<Pair<String, Float>> list, String name);
     public portfolio getPortfolio(String name);
     public void readPortfolioFile(String filename);
-    public String[] getPortfolioNames();
+    public ArrayList<String> getPortfolioNames();
     public String[] getPortfolioValue(String name, Date date);
 
 }
