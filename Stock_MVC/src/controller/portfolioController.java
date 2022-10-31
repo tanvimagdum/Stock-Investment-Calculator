453f92b6ff27c080bfd135f4889bf27766c5557c
JamesOMcKee <59444223+JamesOMcKee@users.noreply.github.com>
2022-10-31 09:02:53 -0400

    Monday Start

--- Stock_MVC/src/controller/portfolioController.java
+++ Stock_MVC/src/controller/portfolioController.java
@@ -2,7 +2,10 @@ package controller;
 
 import model.Pair;
 import model.portfolio;
+import view.viewInterface;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -11,12 +14,14 @@ public interface portfolioController {
 
     public void portBuilder(ArrayList<Pair<String, Float>> list, String name);
     public portfolio getPortfolio(String name);
-    public void readPortfolioFile(String filename);
+    public void readPortfolioFile(String filename) throws FileNotFoundException;
+    public void savePortfolio(String filename) throws IOException;
     public String[] getPortfolioNames();
+    public String selectPortfolio(viewInterface view);
     public String[] getPortfolioValue(String name, Date date);
-
     public String[] getPortfolioContents(String name);
-
+    public void buildPortfolio(viewInterface v);
+    public String[] manualValuation(String name, viewInterface v);
     public String[] getTickers(String name);
     public Float[] getCounts(String name);
 
--- Stock_MVC/src/model/portfolioManager.java
+++ Stock_MVC/src/model/portfolioManager.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -10,7 +12,8 @@ public interface portfolioManager {
 
   public void portBuilder(ArrayList<Pair<String, Float>> list, String name);
   public portfolio getPortfolio(String name);
-  public void readPortfolioFile(String filename);
+  public void readPortfolioFile(String filename) throws FileNotFoundException;
+  public void savePortfolio(String portfolioName) throws IOException;
   public String[] getPortfolioNames();
   public String[] getPortfolioValue(String name, Date date);
   public String[] getPortfolioContents(String name);
