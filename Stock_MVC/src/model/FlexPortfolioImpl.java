65f3c968ee77612d4dda8b829f923624688e5e11
JamesOMcKee <59444223+JamesOMcKee@users.noreply.github.com>
2022-11-09 10:51:39 -0500

    FlexPort basics

--- Stock_MVC/src/model/FlexPortfolioImpl.java
+++ Stock_MVC/src/model/FlexPortfolioImpl.java
@@ -1,4 +1,76 @@
 package model;
 
-public class FlexPortfolioImpl {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class FlexPortfolioImpl implements Portfolio{
+    private ArrayList<FlexStock<String, Float, Date>> stockList;
+    private final String portfolioName;
+
+    public FlexPortfolioImpl(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
+
+    /**
+     * Returns an array list of stocks.
+     *
+     * @return arraylist containing stocks with a
+     * ticker number and count of stocks
+     */
+    @Override
+    public ArrayList<Stock<String, Float>> returnList() {
+        return null;
+    }
+
+    /**
+     * Get the name of portfolio.
+     *
+     * @return the portfolio name as a string
+     */
+    @Override
+    public String getPortfolioName() {
+        return portfolioName;
+    }
+
+    /**
+     * Get the list of ticker numbers in the portfolio.
+     *
+     * @return string array of ticker numbers
+     */
+    @Override
+    public String[] getTickers() {
+        String[] tickers = new String[stockList.size()];
+        for (int i = 0; i < stockList.size(); i++) {
+            tickers[i] = stockList.get(i).getS();
+        }
+        return tickers;
+    }
+
+    /**
+     * Get the list of counts of each stock in the portfolio.
+     *
+     * @return float array of counts of stocks
+     */
+    @Override
+    public Float[] getCounts() {
+        Float[] counts = new Float[stockList.size()];
+        for (int i = 0; i < stockList.size(); i++){
+            counts[i] = stockList.get(i).getF();
+        }
+        return counts;
+    }
+
+    /**
+     * Get the list of purchase dates of each stock in the portfolio.
+     *
+     * @return date array of purchase dates
+     */
+    public Date[] getDates() {
+        Date[] dates = new Date[stockList.size()];
+        for (int i = 0; i < stockList.size(); i++){
+            dates[i] = stockList.get(i).getD();
+        }
+        return dates;
+    }
 }
--- Stock_MVC/src/model/FlexStock.java
+++ Stock_MVC/src/model/FlexStock.java
@@ -1,13 +1,13 @@
 package model;
 
-public class FlexStock<D> extends Stock{
+public class FlexStock<S,F,D> extends Stock<S,F>{
     D dataThree;
 
     public D getD() {
         return dataThree;
     }
 
-    public FlexStock(Object newDataOne, Object newDataTwo, D newDataThree) {
+    public FlexStock(S newDataOne, F newDataTwo, D newDataThree) {
         super(newDataOne, newDataTwo);
         this.dataThree = newDataThree;
     }
