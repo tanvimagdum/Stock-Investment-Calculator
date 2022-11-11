27d13d9eebc52f2d35245b9c66a9fc406f80f487
tanvimagdum <magdum.t@northeastern.edu>
2022-11-11 15:17:04 -0500

    commit 3.17

--- Stock_MVC/src/controller/PortfolioControllerImpl.java
+++ Stock_MVC/src/controller/PortfolioControllerImpl.java
@@ -204,7 +204,7 @@ public class PortfolioControllerImpl implements PortfolioController {
       return name;
     }
 
-    //return model.editFlexPortfolio();
+    //editFlexPortfolio();
     return name;
   }
 
