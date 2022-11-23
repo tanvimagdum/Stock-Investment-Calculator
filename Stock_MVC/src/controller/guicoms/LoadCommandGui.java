e20e3ba4e4b45fc5cd3d7de3ab730e4d5f8f87d6
JamesOMcKee <59444223+JamesOMcKee@users.noreply.github.com>
2022-11-23 13:43:46 -0500

    Removing PortController

---
+++ Stock_MVC/src/controller/guicoms/LoadCommandGui.java
@@ -0,0 +1,13 @@
+package controller.guicoms;
+
+import controller.GuiCommand;
+
+public class LoadCommandGui implements GuiCommand {
+
+  @Override
+  public void go() {
+    //get name from view
+
+    //code to load file into model
+  }
+}
