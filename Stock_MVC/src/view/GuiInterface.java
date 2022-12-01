ba78d4986afb5433de65e1602adceab4fe43aa32
JamesOMcKee <59444223+JamesOMcKee@users.noreply.github.com>
2022-12-01 09:20:34 -0500

    View Gui Commands

--- Stock_MVC/src/view/GuiInterface.java
+++ Stock_MVC/src/view/GuiInterface.java
@@ -2,4 +2,15 @@ package view;
 
 public interface GuiInterface extends ViewInterface {
 
+  public String printWarning(String line);
+
+  public Object[] getOperationalStuff();
+
+  public String getPortfolioName();
+
+  public void setConStuff(Object[] o);
+
+  public void setCurrScreen(String str);
+
+  public String getCurrScreen();
 }
